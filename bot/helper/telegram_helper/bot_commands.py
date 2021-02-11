2875773c9a4036d15c10a59cdd52f94d6ade9b13
getreyu <76866622+getreyu@users.noreply.github.com>
2021-02-11 21:47:04 +0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,9 +1,9 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'mir'
-        self.UnzipMirrorCommand = 'unzip'
-        self.TarMirrorCommand = 'tarmir'
+        self.MirrorCommand = 'mirror'
+        self.UnzipMirrorCommand = 'unzipmirror'
+        self.TarMirrorCommand = 'tarmirror'
         self.CancelMirror = 'cancel'
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
@@ -19,8 +19,8 @@ class _BotCommands:
         self.HelpCommand = 'help'
         self.LogCommand = 'log'
         self.CloneCommand = "clone"
-        self.WatchCommand = 'yt'
-        self.TarWatchCommand = 'taryt'
+        self.WatchCommand = 'watch'
+        self.TarWatchCommand = 'tarwatch'
         self.deleteCommand = 'del'
 
 BotCommands = _BotCommands()
